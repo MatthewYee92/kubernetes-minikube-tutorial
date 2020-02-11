8ed5ccd8ef1e456e41ac4e65b8fef1431a0e2d46
Matthew Yee <matthew.yee@ibm.com@Matthews-MacBook-Pro.local>
2020-02-11 08:08:11 -0600

    updated tutorial repo.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var http = require('http');
+let http = require('http');
 
 var handleRequest = function(request, response) {
 console.log('Received request for URL: ' + request.url);
 response.writeHead(200);
-response.end('Namaste World!');
+response.end('Hello K8');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
